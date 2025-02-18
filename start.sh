57a47d622fe79d963c3b89eae44541e59e12e6d8
Kienakathatguy <busterkgaming@gmail.com>
2025-02-18 17:06:08 +0000

    AutoCorrection 2

--- start.sh
+++ start.sh
@@ -4,8 +4,5 @@
 sudo apt-get update
 sudo apt-get install -y default-jre
 
-# Install Python dependencies
-pip install -r requirements.txt
-
 # Start your web application (replace with the actual command)
 python AutoCorrect.py
