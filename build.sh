dbcb05aeb06f6161a41212bbcd6ff28f2d4197f5
Kienakathatguy <busterkgaming@gmail.com>
2025-02-19 16:05:47 +0000

    Final 3

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 curl -fsSL https://adoptium.net/releases.html | grep -oP 'https://.*?OpenJDK.*?x64_Linux.tar.gz' | head -1 | xargs curl -O
 tar -xzf OpenJDK*.tar.gz
 export PATH=$PWD/jdk/bin:$PATH
+
+pip install -r requirements.txt
