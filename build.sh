b507aee854aaf0c47e0bb4ffd95468333517dc3f
Kienakathatguy <busterkgaming@gmail.com>
2025-02-19 16:03:32 +0000

    Final 2

---
+++ build.sh
@@ -0,0 +1,4 @@
+# Install Java in Render
+curl -fsSL https://adoptium.net/releases.html | grep -oP 'https://.*?OpenJDK.*?x64_Linux.tar.gz' | head -1 | xargs curl -O
+tar -xzf OpenJDK*.tar.gz
+export PATH=$PWD/jdk/bin:$PATH
