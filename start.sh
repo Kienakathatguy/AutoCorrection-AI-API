22a4530fe9f8fa292150dea2db916f44fddc86ac
Kienakathatguy <busterkgaming@gmail.com>
2025-02-19 16:23:05 +0000

    Final 4

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# Install Java if needed
-sudo apt-get update
-sudo apt-get install -y default-jre
-
-# Start your web application (replace with the actual command)
 python AutoCorrect.py
