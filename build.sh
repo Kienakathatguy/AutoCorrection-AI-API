ad37b67d3967a9421b476c2bb7026c728dc29cb2
Kienakathatguy <busterkgaming@gmail.com>
2025-03-28 00:37:40 +0000

    Update deployment

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env bash
-
-# Ensure the system is updated
-apt-get update && apt-get install -y openjdk-17-jdk
-
-# Verify that Java is installed
-java -version
-
+curl -o jre.tar.gz https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
+mkdir -p /opt/java
+tar -xzf jre.tar.gz -C /opt/java --strip-components=1
+export JAVA_HOME=/opt/java
+export PATH=$JAVA_HOME/bin:$PATH
 pip install -r requirements.txt
